6822b8c3ccca3c878e6d1fe47d22e254d6b6389c
tewanouon <43531887+tewanouon@users.noreply.github.com>
2019-04-24 15:29:40 +0700

    Add files via upload

---
+++ array_index.js
@@ -0,0 +1,16 @@
+//Array.prototype.reduce() in es6 style
+
+
+
+const indexOfAll = (arr, val) => arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), []);
+
+
+
+const cars = ["Honda", "Volvo", "BMW", "Honda", "Honda", "Toyota"];
+ 
+
+console.log(indexOfAll(cars, "Honda")); 
+
+
+
+
---
+++ recursive_decode_strings.js
@@ -0,0 +1,23 @@
+
+//recursive decode strings
+function decodeString(str){
+
+	let regex = /(\d+)\[([a-z]*)\]/gi;
+
+	let result = str.replace(regex, (match, num, substr) => substr.repeat(num));
+
+	let recall = regex.test(result);
+	
+        if(recall){
+		
+           return decodeString(result);
+
+        }
+	
+        return result;
+
+ }
+
+
+
+//console.log( decodeString("2[3[a]b]") );
